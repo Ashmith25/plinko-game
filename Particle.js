57c52ec2ae95e2a0fb3a1f2e0a5d9e66e1d924e7
Ashmith25 <77400107+Ashmith25@users.noreply.github.com>
2021-05-19 21:48:15 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,16 @@
+class particle {
+    constructor(x, y, r) {
+        var options = {
+
+            isStatic: true
+        }
+        this.body = Bodies.circle(x, y, this.r, options);
+        this.r=r;
+        this.color=color(random(0,255),random(0,255),random(0,255));
+        World.add(world, this.body);
+    }
+    display() {
+        var pos = this.body.position;
+        (pos.x, pos.y, this.r);
+    }
+};
